f21c14946d6010f29cd1ba7d64b88b6d83742fc0
Eultype <Sdarryy59@gmail.com>
2025-06-30 12:24:23 +0200

    Public page done

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,1 +1,32 @@
 <?php
+# TI3-2025-Sam/controller/publicController.php
+
+// Dépendances
+require "../model/utilisateursModel.php";
+require "../model/localisationsModel.php";
+
+if (isset($_GET['pg'])) {
+    if ($_GET['pg'] === 'login') {
+        // Page de connexion
+        if (isset($_POST['login'], $_POST['userpwd'])) {
+            // tentative de connexion
+            $connect = connectUser($db, $_POST['login'], $_POST['userpwd']);
+            if ($connect === true) {
+                // redirection vers l'accueil
+                header("Location: ./");
+                exit();
+            } else {
+                $error = "Login et/ou mot de passe incorrect";
+            }
+        }
+        // appel du formulaire
+        require_once "../view/public/login.html.php";
+    } else {
+        $points = getLocalisations($db);
+        require_once "../view/public/homepage.html.php";
+    }
+} else {
+    $points = getLocalisations($db);
+    // Chargement des localisations pour l'accueil
+    require_once "../view/public/homepage.html.php";
+}
--- model/localisationsModel.php
+++ model/localisationsModel.php
@@ -1,1 +1,16 @@
 <?php
+# TI3-2025-Sam/model/localisationsModel.php
+
+// Fonction pour récupérer les localisations
+function getLocalisations(PDO $connect): array {
+    $sql  = " SELECT * FROM localisations ";
+    try{
+        $query = $connect->query($sql);
+        $result = $query->fetchAll();
+        $query->closeCursor();
+        return $result;
+    } catch (Exception $e){
+        die($e->getMessage());
+    }
+}
+
