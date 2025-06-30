13a6b5e5fc280cec9bac2f830eafa57925d6a917
NikGorban <masarash12345@gmail.com>
2025-06-30 15:25:05 +0200

    publicController

--- controller/publicController.php
+++ controller/publicController.php
@@ -6,3 +6,38 @@ $locations = getAllLocations($db);
 $jlocations = json_encode($locations);
 require_once "../view/public/home.php";
 
+if(isset($_GET['pg'])){
+    if($_GET['pg']==="username"){
+        // création des variables qui affichent le succès/ erreur de connexion
+        
+        $displaySucces = "d-none"; // non visible par défaut 
+        $displayError = "d-none"; // non visible par défaut 
+        $displayForm = ""; // affichage du formulaire visible par défaut 
+        // ici tentative de connexion
+       
+        if(isset($_POST['username']) && isset($_POST['passwd'])){
+            // si c'est le bon utilisateur
+           
+            if(connectUser($db,$_POST['username'],$_POST['passwd'])){
+                $displaySucces = ""; // succès visible 
+                $displayForm = "d-none"; // on cache le formulaire 
+                // création d'un javascript de redirection
+              
+                $jsRedirect = "<script>
+                    setTimeout(() => {
+                        window.location.href = './';
+                }, 3000); // Redirects after 3 seconds 
+                                </script>";
+            }else{
+                // affichage de l'erreur
+              
+                $displayError = "";
+            }
+        }
+
+        // appel de la vue
+       
+        require_once "../view/login.html.php";
+    }
+}
+
--- model/utilisateursModel.php
+++ model/utilisateursModel.php
@@ -1,6 +1,71 @@
 <?php
 
 # Connexion de l'administrateur en utilisant password_verify
+function connectUser(PDO $con, string $userLogin, string $userPwd): bool
+{
+    // on va protéger des espaces au début et à la fin
+    // des variables de connexions (copier/coller).
+    
+    $userPwd = trim($userPwd);
+    // requête préparée que sur le login (champ unique)
+    
+    $request = $con->prepare("SELECT * FROM `utilisateurs` WHERE `username`= ?");
+    try{
+        $request->execute([$userLogin]);
+        // on a récupéré personne
+      
+        if($request->rowCount()===0) return false;
+        // on a donc UN utilisateur (champ unique),
+        // transformation en tableau associatif
+       
+        // bonne pratique
+  
+        $request->closeCursor();
+        // on va vérifier son mot de passe
+        // entre celui passé par le formulaire et celui venant de la DB
+       
+        if(password_verify($userPwd,$result['passwd'])){
+            // on met en session tout ce qu'on a été récupéré de la requête
+            // tableau associatif = tableau associatif
+          
+            $_SESSION = $result;
 
+            // suppression du mot de passe
+            // удаляем пароль
+            unset($_SESSION['passwd']);
+            return true;
+        }else{
+            return false;
+        }
+
+    }catch (Exception $e){
+        die($e->getMessage());
+    }
+}
 
 # Déconnexion de l'administrateur
+
+function disconnectUser(): bool
+{
+    # suppression des variables de sessions
+ 
+    session_unset();
+
+    # suppression du cookie côté browser / utilisateur
+   
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    # Destruction du fichier lié sur le serveur
+    
+    session_destroy();
+
+    // envoi de true pour éviter un comportement asynchrone
+    
+    return true;
+}
