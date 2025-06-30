9c386806fcecf30b2054320e889d8673c6e9e2a3
vahag1991 <smbatyanvahagn1991@gmail.com>
2025-06-30 10:44:48 +0200

    selectAllLocation

--- model/localisationsModel.php
+++ model/localisationsModel.php
@@ -1,1 +1,41 @@
 <?php
+
+/**
+ * @param PDO $connexion
+ * @return array
+ * Sélection de tous location
+ * 
+ */
+
+ function selectAllLocalisation(PDO $connexion): array | bool
+{
+    $sql = "SELECT * FROM `localisations`";
+    $prepare = $connexion->prepare($sql);
+
+    try{
+       $recup = $prepare->fetchAll();
+       $prepare->closeCursor();
+       return $recup;
+
+    }catch(Exception $e){
+        die($e->getMessage());
+    }
+}
+
+function selectLocalisationById(PDO $connexion, int $id): array | bool
+{
+    $sql = "SELECT * FROM `localisations` WHERE `id` = :id";
+    $prepare = $connexion->prepare($sql);
+
+    try {
+        $prepare->execute([':id' => $id]);
+        $result = $prepare->fetch(PDO::FETCH_ASSOC);
+        $prepare->closeCursor();
+        return $result;
+    } catch (Exception $e) {
+        die("Erreur lors de la récupération de la localisation : " . $e->getMessage());
+    }
+}
+
+
+
