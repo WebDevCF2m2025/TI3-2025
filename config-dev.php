aa49564f9e689ffd360a0341e51a528508026265
NikGorban <masarash12345@gmail.com>
2025-06-30 09:56:34 +0200

    PDO

---
+++ config-dev.php
@@ -0,0 +1,13 @@
+<?php
+
+# MySQL
+const DB_HOST = "localhost";
+const DB_NAME = "ti3_2025";
+const DB_LOGIN = "root";
+const DB_PWD = "";
+const DB_PORT = 3307; // MariaDB
+const DB_CHARSET = "utf8mb4";
+
+const APP_MODE = "dev";
+
+const DB_DSN = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=".DB_CHARSET;
