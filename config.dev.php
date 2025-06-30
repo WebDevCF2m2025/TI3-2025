6d71b5a965fd27bc33f5906ee3e79050d127c98c
agim <agim.coroli8016@gmail.com>
2025-06-30 09:58:38 +0200

    remplissage config/config-dev, controlleur frontal

---
+++ config.dev.php
@@ -0,0 +1,10 @@
+<?php 
+# MySQL
+const DB_HOST = "localhost";
+const DB_NAME = "TI3_2025";
+const DB_LOGIN = "root";
+const DB_PWD = "";
+const DB_PORT = 3306;
+const DB_CHARSET = "utf8mb4";
+
+const DB_DSN = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT.";charset=".DB_CHARSET;
