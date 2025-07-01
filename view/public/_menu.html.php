14d1f4e47a356eee8fd21b946d851758508ac4e5
Nono1050 <abdelkadernordine@hotmail.com>
2025-07-01 14:43:29 +0200

    modification navbar admin

--- view/public/_menu.html.php
+++ view/public/_menu.html.php
@@ -1,18 +1,26 @@
-<ul class="navbar-nav nav me-auto ps-lg-5 mb-2 mb-lg-0">
-    <li class="nav-item"><a class="nav-link scroll-link " aria-current="page" href="./">Accueil</a></li>
-   
-    <?php
-    // si connecté
-    if(isset($_SESSION['username'])):
-    ?>
-        <li class="nav-item"><a class="nav-link scroll-link  " href="./?pg=admin">Administration</a></li>
-        <li class="nav-item"><a class="nav-link scroll-link" href="./?pg=disconnect">Déconnexion de <?=$_SESSION['username']?></a></li>
-    <?php
-    // pas connecté
-    else:
-    ?>
-    <li class="nav-item"><a class="nav-link scroll-link" href="./?pg=login">Connexion</a></li>
-    <?php
-    endif;
-    ?>
-</ul>
+
+
+
+
+
+<nav class="d-flex w-100">
+    <ul class="navbar-nav d-flex flex-row me-auto w-auto">
+        <li class="nav-item">
+            <a class="nav-link scroll-link" aria-current="page" href="./"><strong>Accueil</strong></a>
+        </li>
+    </ul>
+    <ul class="navbar-nav d-flex flex-row ms-auto w-auto">
+        <?php if(isset($_SESSION['username'])): ?>
+            <li class="nav-item">
+                <a class="nav-link scroll-link" href="./?pg=admin"><strong>Administration</strong></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link scroll-link" href="./?pg=disconnect"><strong>Déconnexion</strong></a>
+            </li>
+        <?php else: ?>
+            <li class="nav-item">
+                <a class="nav-link scroll-link" href="./?pg=login"><strong>Connexion</strong></a>
+            </li>
+        <?php endif; ?>
+    </ul>
+</nav>
