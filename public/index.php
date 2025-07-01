f26a49df6f43733e14f94114e3da634cb877d890
OkiSenpai <omersatelit@gmail.com>
2025-07-01 15:35:28 +0200

    vardump deleted

--- public/index.php
+++ public/index.php
@@ -33,16 +33,3 @@ if (isset($_SESSION['login'])) {
 }
 
 
-echo '<div class="bg-light py-4 mt-auto">
-   <h3> débogage</h3>';
-echo '<h4>session_id() ou SID</h4>';
-var_dump(session_id());
-echo '<h4>$_GET</h4>';
-var_dump($_GET);
-echo '<h4>$_SESSION</h4>';
-var_dump($_SESSION);
-echo '<h3>$_POST</h3>';
-var_dump($_POST);
-echo '</div></div>';
-?>
-<h1>HELO</h1>
