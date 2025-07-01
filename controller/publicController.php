31961ef01310aa3e8faa528e6564758e793550c7
OkiSenpai <omersatelit@gmail.com>
2025-07-01 09:31:21 +0200

    header when log redirection

--- controller/publicController.php
+++ controller/publicController.php
@@ -7,18 +7,18 @@ if (isset($_GET['page']) && $_GET['page'] === 'login') {
     header("Location: ../view/public/login.php");
 
     if (isset($_POST['login']) && isset($_POST['password'])) {
-        
+
         $login = $_POST['login'];
         $password = $_POST['password'];
 
         if (authentificateActivedUser($db, $login, $password)) {
-            header("Location: ../view/public/home.php");
+            header("Location: ../controller/privateController.php");
             exit;
         } else {
             echo "Login failed. Please try again.";
         }
     }
-   
+
 }
 
 require_once "../view/public/home.php";
--- view/public/login.php
+++ view/public/login.php
@@ -1,29 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <title>login</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+  <title>login</title>
 </head>
+
 <body>
-    <form class="container mt-5 w-25" method="post" action="">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="login">
+  <form class="container mt-5 w-25" method="post" action="">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="login">
 
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+    crossorigin="anonymous"></script>
 </body>
+
 </html>
+
+<?php
+echo '<div class="bg-light py-4 mt-auto">
+   <h3> débogage</h3>';
+echo '<h4>session_id() ou SID</h4>';
+var_dump(session_id());
+echo '<h4>$_GET</h4>';
+var_dump($_GET);
+echo '<h4>$_SESSION</h4>';
+var_dump($_SESSION);
+echo '<h3>$_POST</h3>';
+var_dump($_POST);
+echo '</div></div>';
+?>
